7010a4b3c0ada6b870a9d45c19c5f60e2d54e9f4
Joseph Karasek and Mary Warrington <joseph.karasek@gmail.com>
2016-03-30 13:56:55 -0700

    Add post route to view single post

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,11 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
-  },
-  actions: {
-    delete(post) {
-      post.destroyRecord();
-      this.transitionTo('index');
-    }
   }
 });
