a7df579b65ee2a5668aa79b30135b028524958a6
Joseph Karasek and Mary Warrington <marywarrington@gmail.com>
2016-03-31 10:17:58 -0700

    add empty comment list and comment tile components

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,9 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr(),
   author: DS.attr(),
   body: DS.attr(),
-  image: DS.attr(),
-  comments: DS.hasMany('comment', { async: true})
+  post: DS.belongsTo('post', { async: true})  
 });
